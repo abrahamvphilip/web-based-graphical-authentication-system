79489a8f2d6d8c0fa41156a0fb31eec41b805a34
philipofficial <macphilip6@gmail.com>
2021-09-26 15:53:35 +0530

    Create project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Home from './components/home-container/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
