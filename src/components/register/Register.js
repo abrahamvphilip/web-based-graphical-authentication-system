e41444c9b5cc1990c03e7a6fa2f79f6ab41e3294
philipofficial <macphilip6@gmail.com>
2021-09-29 20:45:17 +0530

    register component almost done

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -3,8 +3,35 @@ import './Register.css'
 
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="auth-register">
+      <div className="top">
+          <div className="project-name">
+            Web based Graphical <br /> Authentication System
+          </div>
+          <div className="current-page">
+            Register
+          </div>
+        </div>
+        <div className="auth-container">
+          <div className="auth-text">
+            Register
+          </div>
+          <div className="auth-form">
+            <form>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Username" />
+              <input type="email" placeholder="Email" />
+              <div className="auth-image-upload">
+                {/* Uploader code goes here */}
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="creator-detail">
+            <p className="creator">A project by Khushi <br /> and Philip</p>
+          </div>
+        </div>
     </div>
   )
 }
